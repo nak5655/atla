f7afd2912eae92ed88e794ecb7887561142bf442
Nak <2912067+nak5655@users.noreply.github.com>
2026-04-20 19:49:08 +0900

    fix lowering test

--- src/Atla.Core.Tests/Lowering/LoweringTests.fs
+++ src/Atla.Core.Tests/Lowering/LoweringTests.fs
@@ -667,7 +667,7 @@ import System.Text.StringBuilder
 import System.Console
 
 fn process (sb: StringBuilder): () = do
-    sb.Append "ok"
+    let _ = sb.Append "ok"
     Console.WriteLine (sb.ToString ())
 
 fn main: () = do
