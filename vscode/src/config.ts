c8767d970cf1001beafab217a7a5cb32b75de6de
Nak <2912067+nak5655@users.noreply.github.com>
2026-04-23 21:06:55 +0900

    Add VSCode extension

---
+++ vscode/src/config.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+// TODO 環境変数の置換の仕方が分からないのでとりあえず決め打ちで
+function replacePathVar(path: string): string {
+    return path.replace('~', process.env['USERPROFILE']!);
+}
+
+export class Config {
+    readonly rootSection = 'atla';
+
+    constructor(_: vscode.ExtensionContext) {}
+
+    private get<T>(path: string): T {
+        return this.cfg.get<T>(path)!;
+    }
+
+    private get cfg(): vscode.WorkspaceConfiguration {
+        return vscode.workspace.getConfiguration(this.rootSection);
+    }
+
+    get serverPath() {
+        return replacePathVar(this.get<string>('lsp-server.path'));
+    }
+
+    get buildSystemPath() {
+        return replacePathVar(this.get<string>('build-system.path'));
+    }
+}
---
+++ vscode/src/extension.ts
@@ -0,0 +1,74 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as lc from 'vscode-languageclient/node';
+import { Config } from './config';
+import { activateTaskProvider } from './tasks';
+
+let client: lc.LanguageClient;
+
+// 拡張機能が有効になったときに呼ばれる
+export async function activate(context: vscode.ExtensionContext) {
+    await tryActivate(context).catch((err) => {
+        void vscode.window.showErrorMessage(`Cannot activate Atla: ${err.message}`);
+        throw err;
+    });
+}
+
+async function tryActivate(context: vscode.ExtensionContext) {
+    const config = new Config(context);
+
+    // コマンド登録
+    const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+    if (workspaceFolder === undefined) {
+        throw new Error('no folder is opened');
+    }
+    context.subscriptions.push(activateTaskProvider(workspaceFolder, config));
+
+    // サーバーのパスを取得
+    const serverPath = config.serverPath;
+
+    // サーバーの設定
+    const run: lc.Executable = {
+        command: serverPath,
+        options: { env: process.env },
+    };
+    const serverOptions: lc.ServerOptions = {
+        run,
+        debug: run,
+    };
+    // LSPとの通信に使うリクエストを定義
+    const clientOptions: lc.LanguageClientOptions = {
+        // 対象とするファイルの種類や拡張子
+        documentSelector: [{ scheme: 'file', language: 'atla' }],
+        // 警告パネルでの表示名
+        diagnosticCollectionName: 'Atla',
+        revealOutputChannelOn: lc.RevealOutputChannelOn.Never,
+        initializationOptions: {},
+        progressOnInitialization: true,
+    };
+
+    try {
+        // LSPを起動
+        client = new lc.LanguageClient('Atla LSP Server', serverOptions, clientOptions);
+    } catch (err) {
+        void vscode.window.showErrorMessage(
+            'Failed to launch Atla LSP Server. See output for more details.',
+        );
+        return;
+    }
+    client.start().catch((error) => client.error(`Starting the server failed.`, error, 'force'));
+
+    // 通知
+    vscode.workspace.onDidChangeConfiguration(
+        (_) => client.sendNotification('workspace/didChangeConfiguration', { settings: '' }),
+        null,
+        context.subscriptions,
+    );
+}
+
+export async function deactivate(): Promise<void> {
+    if (client) {
+        await client.stop();
+    }
+}
