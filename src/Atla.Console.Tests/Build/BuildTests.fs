ca09c49590d54d968cf9d5a46a67fa09b3f7faf9
Nak <2912067+nak5655@users.noreply.github.com>
2026-05-01 20:02:30 +0900

    delete data example

--- src/Atla.Console.Tests/Build/BuildTests.fs
+++ src/Atla.Console.Tests/Build/BuildTests.fs
@@ -114,22 +114,6 @@ fn main: () = do
             Assert.True(false, "Repository root was not found from current working directory.")
 
     [<Fact>]
-    let ``build should succeed for examples data`` () =
-        match tryFindRepositoryRoot () with
-        | Some repositoryRoot ->
-            let projectRoot = Path.Join(repositoryRoot, "examples", "data")
-            let outDir = Path.Join(projectRoot, "out-regression")
-
-            if Directory.Exists(outDir) then
-                Directory.Delete(outDir, recursive = true)
-
-            let code = Console.run [| "build"; projectRoot; "-o"; outDir; "--name"; "DataExampleRegression" |]
-            Assert.Equal(0, code)
-            Assert.True(File.Exists(Path.Join(outDir, "DataExampleRegression.dll")))
-        | None ->
-            Assert.True(false, "Repository root was not found from current working directory.")
-
-    [<Fact>]
     let ``build should succeed for examples hello_module`` () =
         match tryFindRepositoryRoot () with
         | Some repositoryRoot ->
